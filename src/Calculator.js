5bd7f92b87eb99b89cb62166681c7eb14a624bb8
John U <arjorb@gmail.com>
2023-03-13 17:14:39 +0200

    impliment handleEqual function and the handleOperator function

--- src/Calculator.js
+++ src/Calculator.js
@@ -5,6 +5,42 @@ const Calculator = () => {
   const [operand, setOperand] = useState(null);
   const [prevValue, setPrevValue] = useState(null);
 
+  const handleNumberButtons = number => {
+    if (display === '0') {
+      setDisplay(number);
+    } else {
+      setDisplay(display + number);
+    }
+  };
+
+  const handleOperator = operator => {
+    setPrevValue(parseFloat(display));
+    setOperand(operator);
+    setDisplay('0');
+  };
+
+  const handleEqual = () => {
+    const current = parseFloat(display);
+    let result;
+
+    switch (operand) {
+      case '+':
+        result = prev + current;
+        break;
+      case '-':
+        result = prev - current;
+        break;
+      case '*':
+        result = prev * current;
+        break;
+      case '/':
+        result = prev / current;
+        break;
+      default:
+        result = current;
+    }
+  };
+
   return (
     <div className='mt-10 w-96 shadow-xl'>
       <div className='h-16 bg-gray-500'></div>
@@ -16,7 +52,9 @@ const Calculator = () => {
         <button className='w-24 h-24 bg-yellow-500 border border-r-0 border-gray-300'>/</button>
       </div>
       <div>
-        <button className='w-24 h-24 bg-gray-200 border border-l-0 border-gray-300'>7</button>
+        <button className='w-24 h-24 bg-gray-200 border border-l-0 border-gray-300' onClick={() => handleNumberButtons(7)}>
+          7
+        </button>
         <button className='w-24 h-24 bg-gray-200 border border-gray-300'>8</button>
         <button className='w-24 h-24 bg-gray-200 border border-gray-300'>9</button>
         <button className='w-24 h-24 bg-yellow-500 border border-r-0 border-gray-300'>x</button>
