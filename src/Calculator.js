f6a6a5cb8a201a7effbdd4b45e16e0d668a51552
John U <arjorb@gmail.com>
2023-03-13 17:36:06 +0200

    fixing the problem in the handleNumberButton function of string

--- src/Calculator.js
+++ src/Calculator.js
@@ -3,18 +3,20 @@ import { useState } from 'react';
 const Calculator = () => {
   const [display, setDisplay] = useState('0');
   const [operand, setOperand] = useState(null);
-  const [prevValue, setPrevValue] = useState(null);
+  const [prev, setPrev] = useState(null);
 
   const handleNumberButtons = number => {
     if (display === '0') {
       setDisplay(number);
     } else {
-      setDisplay(display + number);
+      let result = display.toString();
+      setDisplay(result + number);
     }
   };
 
+  console.log(display);
   const handleOperator = operator => {
-    setPrevValue(parseFloat(display));
+    setPrev(parseFloat(display));
     setOperand(operator);
     setDisplay('0');
   };
@@ -39,14 +41,34 @@ const Calculator = () => {
       default:
         result = current;
     }
+
+    setPrev(null);
+    setOperand(null);
+    setDisplay(result.toString());
+  };
+
+  const handleDecimal = () => {
+    if (!display.includes('.')) {
+      setDisplay(display + '.');
+    }
+  };
+
+  const handleClear = () => {
+    setDisplay('0');
+    setOperand(null);
+    setPrev(null);
   };
 
   return (
     <div className='mt-10 w-96 shadow-xl'>
-      <div className='h-16 bg-gray-500'></div>
+      <div className='h-16 bg-gray-500 text-white font-bold relative'>
+        <div className='absolute right-2 top-2 text-5xl'>{display}</div>
+      </div>
 
       <div>
-        <button className='w-24 h-24 bg-gray-200 border border-l-0 border-gray-300'>AC</button>
+        <button className='w-24 h-24 bg-gray-200 border border-l-0 border-gray-300' onClick={handleClear}>
+          AC
+        </button>
         <button className='w-24 h-24 bg-gray-200 border border-gray-300'>+/-</button>
         <button className='w-24 h-24 bg-gray-200 border border-gray-300'>%</button>
         <button className='w-24 h-24 bg-yellow-500 border border-r-0 border-gray-300'>/</button>
@@ -55,24 +77,42 @@ const Calculator = () => {
         <button className='w-24 h-24 bg-gray-200 border border-l-0 border-gray-300' onClick={() => handleNumberButtons(7)}>
           7
         </button>
-        <button className='w-24 h-24 bg-gray-200 border border-gray-300'>8</button>
-        <button className='w-24 h-24 bg-gray-200 border border-gray-300'>9</button>
+        <button className='w-24 h-24 bg-gray-200 border border-gray-300' onClick={() => handleNumberButtons(8)}>
+          8
+        </button>
+        <button className='w-24 h-24 bg-gray-200 border border-gray-300' onClick={() => handleNumberButtons(9)}>
+          9
+        </button>
         <button className='w-24 h-24 bg-yellow-500 border border-r-0 border-gray-300'>x</button>
       </div>
       <div>
-        <button className='w-24 h-24 bg-gray-200 border border-l-0 border-gray-300'>4</button>
-        <button className='w-24 h-24 bg-gray-200 border border-gray-300'>5</button>
-        <button className='w-24 h-24 bg-gray-200 border border-gray-300'>6</button>
+        <button className='w-24 h-24 bg-gray-200 border border-l-0 border-gray-300' onClick={() => handleNumberButtons(4)}>
+          4
+        </button>
+        <button className='w-24 h-24 bg-gray-200 border border-gray-300' onClick={() => handleNumberButtons(5)}>
+          5
+        </button>
+        <button className='w-24 h-24 bg-gray-200 border border-gray-300' onClick={() => handleNumberButtons(6)}>
+          6
+        </button>
         <button className='w-24 h-24 bg-yellow-500 border border-r-0 border-gray-300'>-</button>
       </div>
       <div>
-        <button className='w-24 h-24 bg-gray-200 border border-l-0 border-gray-300'>1</button>
-        <button className='w-24 h-24 bg-gray-200 border border-gray-300'>2</button>
-        <button className='w-24 h-24 bg-gray-200 border border-gray-300'>3</button>
+        <button className='w-24 h-24 bg-gray-200 border border-l-0 border-gray-300' onClick={() => handleNumberButtons(1)}>
+          1
+        </button>
+        <button className='w-24 h-24 bg-gray-200 border border-gray-300' onClick={() => handleNumberButtons(2)}>
+          2
+        </button>
+        <button className='w-24 h-24 bg-gray-200 border border-gray-300' onClick={() => handleNumberButtons(3)}>
+          3
+        </button>
         <button className='w-24 h-24 bg-yellow-500 border border-r-0 border-gray-300'>+</button>
       </div>
       <div>
-        <button className=' w-48 h-24 bg-gray-200 border border-l-0 border-gray-300'>0</button>
+        <button className=' w-48 h-24 bg-gray-200 border border-l-0 border-gray-300' onClick={() => handleNumberButtons(0)}>
+          0
+        </button>
         <button className='w-24 h-24 bg-gray-200 border border-gray-300'>.</button>
         <button className='w-24 h-24 bg-yellow-500 border border-r-0 border-gray-300'>=</button>
       </div>
